f557cf38951c4c96c86af508ef7f740e2d088967
aarias <ariasaxel98@gmail.com>
2022-09-21 23:09:11 -0300

    se agrega js de carrito de compras de productos

--- Js/productos.js
+++
@@ -1,8 +0,0 @@
-const botonesGet = document.getElementsByClassName('buttonFav'); //Obtengo los HTMLelements
-const botones = Array.prototype.slice.call(botonesGet); //Los paso a un array.
-
-botones.forEach( element => {
-    element.onclick = () => {
-        alert("¡Has agregado esta planta a favoritos!")
-    }
-})
